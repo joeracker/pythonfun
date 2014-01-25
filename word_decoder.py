c772b62c9b59fd038d858242e07b0f697db2f2d3
Joe <jmcbride@rackspace.com>
2014-01-25 07:10:49 -0600

    initial translation complete. need to look at string.maketrans()

--- word_decoder.py
+++ word_decoder.py
@@ -3,12 +3,24 @@ import string
 
 s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
-abc = [ x for x in string.ascii_lowercase][::-1]
+abc = string.ascii_lowercase
+# deal with the wrap around
+abc = abc + "ab"
+#abc = [ x for x in abc]
+print abc
 
+new_phrase = ""
 print s
 x = 0
-for c in abc:
-	if (c in s) and (x<20):
-		s = s.replace(c,abc[x+2])
-		print c + " - " + s
+for c in s:
+	if c in abc:
+		replace_c_with = abc[abc.index(c)+2]
+		new_phrase = new_phrase + replace_c_with
+		print s[x] + " to " + replace_c_with
+		#s = s.replace(s[x],replace_c_with)
+	else:
+		print c + " to " + c
+		new_phrase = new_phrase + c
+	
 	x = x + 1
+print new_phrase
